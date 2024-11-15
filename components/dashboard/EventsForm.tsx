f5c2a284c38e48015ccbc2ad0061ca23980bca16
Focus <mrjoshuaali@yahoo.co.uk>
2024-11-15 09:44:32 +0000

    Add additional categories

--- components/dashboard/EventsForm.tsx
+++ components/dashboard/EventsForm.tsx
@@ -37,6 +37,26 @@ export default function EventsForm({
                 <MenuItem value="Football">Football</MenuItem>
                 <MenuItem value="Basketball">Basketball</MenuItem>
                 <MenuItem value="Tennis">Tennis</MenuItem>
+                <MenuItem value="Cricket">Cricket</MenuItem>
+                <MenuItem value="Golf">Golf</MenuItem>
+                <MenuItem value="Baseball">Baseball</MenuItem>
+                <MenuItem value="Rugby">Rugby</MenuItem>
+                <MenuItem value="Hockey">Hockey</MenuItem>
+                <MenuItem value="Boxing">Boxing</MenuItem>
+                <MenuItem value="MMA">MMA</MenuItem>
+                <MenuItem value="Esports">Esports</MenuItem>
+                <MenuItem value="Cycling">Cycling</MenuItem>
+                <MenuItem value="Table Tennis">Table Tennis</MenuItem>
+                <MenuItem value="Volleyball">Volleyball</MenuItem>
+                <MenuItem value="Motorsport">Motorsport</MenuItem>
+                <MenuItem value="Swimming">Swimming</MenuItem>
+                <MenuItem value="Athletics">Athletics</MenuItem>
+                <MenuItem value="Snooker">Snooker</MenuItem>
+                <MenuItem value="Darts">Darts</MenuItem>
+                <MenuItem value="Badminton">Badminton</MenuItem>
+                <MenuItem value="Handball">Handball</MenuItem>
+                <MenuItem value="Water Polo">Water Polo</MenuItem>
+                <MenuItem value="Wrestling">Wrestling</MenuItem>
             </TextField>
         </>
     );
